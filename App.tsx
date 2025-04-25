d10c52ec5b3c4338b3f30fda503f081e63a1ace0
choocapi <chauminhduong.cmd@gmail.com>
2025-04-25 09:17:56 +0700

    Lab 2

--- App.tsx
+++ App.tsx
@@ -1,18 +1,16 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import HomeScreen from './screens/HomeScreen';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import ContactsNavigator from './screens/Lab2/routes';
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home'>
-        <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+    <NavigationContainer >
+      <Stack.Navigator initialRouteName='Lab2'>
+        <Stack.Screen name="Home" component={HomeScreen} options={{headerShown: false}} />
+        <Stack.Screen name="Lab2" component={ContactsNavigator} options={{headerShown: false}} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({});
